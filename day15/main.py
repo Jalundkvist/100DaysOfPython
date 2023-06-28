04d52ec6b0b8bd258cf6665b0429abd84bcd1782
Jacob Lundkvist <jacob.lundkvist@infotiv.se>
2023-06-28 12:34:29 +0200

    refactor: day15 updated added coin processing and adjusted methods

--- day15/main.py
+++ day15/main.py
@@ -1,4 +1,13 @@
 from data import MENU, resources
+from os import system, name
+from time import sleep
+
+
+def clear_terminal():
+    if name == 'nt':  # For Windows
+        system('cls')
+    else:  # For Unix-like systems (Linux, macOS)
+        system('clear')
 
 
 def print_menu():
@@ -10,22 +19,34 @@ def print_menu():
 def print_resources():
     """Prints the resources and the amount"""
     for resource, amount in resources.items():
-        if resource == "coffee":
-            unit = 'g'
+        if resource == "Money":
+            print(f"{resource.title()}    \t${amount}")
         else:
-            unit = 'ml'
-        print(f"Resource: {resource}  \t{amount}{unit} left")
+            if resource == "coffee":
+                unit = 'g'
+            else:
+                unit = 'ml'
+            print(f"{resource.title()}    \t{amount}{unit}")
 
 
 def check_sufficient_resources(drink):
     """Checks if the coffee machine got sufficient resources for the drink"""
     ingredients = MENU[drink]['ingredients']
+    missing_resource = False
     for resource, amount in ingredients.items():
         if resources.get(resource, 0) < amount:
-            return False
+            print(f'Sorry there is not enough {resource}')
+            missing_resource = True
+    if missing_resource:
+        return False
+    return True
+
+
+def make_drink(drink):
+    """Makes the specified drink"""
+    ingredients = MENU[drink]['ingredients']
     for resource, amount in ingredients.items():
         resources[resource] -= amount
-    return True
 
 
 def get_user_input():
@@ -43,30 +64,57 @@ def get_user_input():
             print('Invalid input!\n')
 
 
-def count_coins(drink):
-    return drink
+def process_coins(drink):
+    cost = MENU[drink]['cost']
+    try:
+        penny = int(input("Enter the number of pennies: ")) * 0.01
+        nickel = int(input("Enter the number of nickels: ")) * 0.05
+        dime = int(input("Enter the number of dimes: ")) * 0.10
+        quarter = int(input("Enter the number of quarters: ")) * 0.25
 
+        # Input validation
+        if any(coin < 0 for coin in [penny, nickel, dime, quarter]):
+            raise ValueError("Number of coins cannot be negative.")
+    except ValueError as e:
+        print(f"Invalid input: {e}")
+        return None
 
-# TODO: Process coins.\
-""" a. If there are sufficient resources to make the drink selected, then the program should
-        prompt the user to insert coins.
-    b. Remember that quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
-    c. Calculate the monetary value of the coins inserted. E.g. 1 quarter, 2 dimes, 1 nickel, 2
-        pennies = 0.25 + 0.1 x 2 + 0.05 + 0.01 x 2 = $0.52
- """
-# TODO: Check transaction successful? I.G Offer change if too much .2f
-
-# TODO: Make Coffee.
+    total_amount = round(penny + nickel + dime + quarter, 2)
+    if total_amount < cost:
+        return None
+    change = round(total_amount - cost, 2)
+    if "Money" not in resources:
+        resources['Money'] = 0
+    resources['Money'] += cost
+    return change
 
 
 def main():
-    print_menu()
-    print_resources()
-    drink = get_user_input()
-    print(drink)
-    if check_sufficient_resources(drink):
-        count_coins(MENU[drink]['cost'])
-        
+    while True:
+        drink, change = None, None
+        drink = get_user_input()
+        if not drink:
+            continue
+
+        ingredients = check_sufficient_resources(drink)
+        if not ingredients:
+            continue
+
+        change = process_coins(drink)
+        if change is None:
+            print(f'Not enough money to brew {drink}!')
+            continue
+
+        make_drink(drink)
+        print(f"Here is your {drink}. Enjoy!")
+
+        if change > 0:
+            change = "{:.2f}".format(change)
+            print(f"Here is your ${change} dollars in change")
+
+        sleep(4)
+        clear_terminal()
+
 
 if __name__ == '__main__':
     main()
