6f7aea046fdf7cea14ae0a438ee9aace99d55361
Jacob Lundkvist <jacob@lundkvist.com>
2023-07-03 17:54:29 +0200

    feat: day17 quizbrain game

--- day17/quiz-game-start/main.py
+++ day17/quiz-game-start/main.py
@@ -2,7 +2,7 @@ from question_model import Question
 from data import question_data
 from quiz_brain import QuizzBrain
 
-question_bank = [Question(question["text"], question["answer"]) for question in question_data]
+question_bank = [Question(question["question"], question["correct_answer"]) for question in question_data]
 quiz_game = QuizzBrain(question_bank)
 
 
