f1833d37dc78d78d804c9786bca04cc5e499e6ce
Jacob Lundkvist <jacob@lundkvist.com>
2023-07-10 21:12:21 +0200

    feat: day20 snake on the move!

--- day20/main.py
+++ day20/main.py
@@ -1,36 +1,30 @@
-from turtle import Screen, Turtle
+from snake import Snake
+from turtle import Screen
 from time import sleep
 
+snake = Snake()
+
+
+def init_screen():
+    game_screen = Screen()
+    game_screen.setup(width=600, height=600)
+    game_screen.bgcolor("black")
+    game_screen.title("Snake game")
+    game_screen.tracer(n=0)
+    game_screen.listen()
+    game_screen.onkey(snake.up, "Up")
+    game_screen.onkey(snake.down, "Down")
+    game_screen.onkey(snake.left, "Left")
+    game_screen.onkey(snake.right, "Right")
+    return game_screen
+
+
 # Screen setup
-screen = Screen()
-screen.setup(width=600, height=600)
-screen.bgcolor("black")
-screen.title("Snake game")
-screen.tracer(n=0)
-
-# Turtle setup
-segments = []
-x, y = 0, 0
-for i in range(5):
-    print(i)
-    segments.append(Turtle(shape="square"))
-    segments[i].color("white")
-    segments[i].pu()
-    segments[i].goto(x, y)
-    x -= 20
-    screen.update()
-sleep(3)
-
-
-def update_position():
-    screen.update()
-    sleep(0.2)
-    for seg_num in range(len(segments) - 1, 0, -1):
-        segments[seg_num].goto(segments[seg_num-1].xcor(), segments[seg_num-1].ycor())
-
+screen = init_screen()
 
 game_on = True
 while game_on:
-    update_position()
-    segments[0].forward(20)
-screen.exitonclick()
+    screen.update()
+    sleep(0.1)
+    snake.move()
+
---
+++ day20/snake.py
@@ -0,0 +1,58 @@
+from turtle import Turtle
+
+START_POSITION = (0, 0)
+MOVE_DISTANCE = 20
+RIGHT, UP, LEFT, DOWN = 0, 90, 180, 270
+SCREEN_RADIUS = 300
+
+
+class Snake:
+
+    def __init__(self):
+        self.snake = []
+        self.init_snake()
+        self.head = self.snake[0]
+
+    def init_snake(self):
+        x, y = START_POSITION
+        for i in range(3):
+            self.add_segment()
+            self.snake[i].goto(x, y)
+            x -= 20
+
+    def add_segment(self):
+        self.snake.append(Turtle(shape="square"))
+        self.snake[-1].color("white")
+        self.snake[-1].pu()
+
+    def move(self):
+        if self.check_boundaries():
+            for seg_num in range(len(self.snake) - 1, 0, -1):
+                self.snake[seg_num].goto(self.snake[seg_num - 1].xcor(), self.snake[seg_num - 1].ycor())
+            self.head.forward(20)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def check_boundaries(self):
+        head = self.head
+        if head.xcor() >= SCREEN_RADIUS or head.xcor() <= -SCREEN_RADIUS:
+            return False
+        elif head.ycor() >= SCREEN_RADIUS or head.ycor() <= -SCREEN_RADIUS:
+            return False
+        return True
+
+
