9a72b1c126c77852821a3982eb4875c27071b0a6
Jacob Lundkvist <jacob@lundkvist.com>
2023-10-05 19:53:45 +0200

    feat: day29 - GUI complete todo: backend

--- day29 - password manager/main.py
+++ day29 - password manager/main.py
@@ -1,6 +1,6 @@
 from tkinter import *
 
-LABELS = ["Website", "Username", "Password"]
+LABELS = ["Website:", "Username:", "Password:"]
 
         
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -12,32 +12,33 @@ LABELS = ["Website", "Username", "Password"]
 # Root window
 root = Tk()
 root.title("pw manager")
-root.config(pady=20, padx=20)
+root.config(pady=25, padx=25)
 
 
 # Logo
 logo = Canvas(height=200, width=200)
 logo_img = PhotoImage(file="logo.png")
-logo.create_image(100, 100, image=logo_img)
-logo.grid(column=1, row=0)
+logo.create_image(70, 100, image=logo_img)
+logo.grid(column=1, row=0, columnspan=2)
 
 # Entries
-entry_website = Entry()
-entry_website.grid(column=1, row=1, columnspan=2)
-entry_username = Entry()
-entry_username.grid(column=1, row=2, columnspan=2)
-entry_password = Entry()
-entry_password.grid(column=1, row=3)
+entry_website = Entry(width=52)
+entry_website.grid(column=1, row=1, columnspan=2, sticky="w")
+entry_username = Entry(width=52)
+entry_username.grid(column=1, row=2, columnspan=2, sticky="w")
+entry_password = Entry(width=33)
+entry_password.grid(column=1, row=3, columnspan=1, sticky="w")
 # Buttons
 button_generate = Button(text="Generate password")
 button_generate.grid(column=2, row=3)
+button_add = Button(text="Add", width=53)
+button_add.grid(column=0, row=4, columnspan=3)
 
 # Labels
 # Create labels for each name and place them in column 0 with increasing row numbers
 for i, name in enumerate(LABELS, start=1):
     label = Label(root, text=name)
     label.grid(row=i, column=0, sticky="w")
-
 
 
 root.mainloop()
